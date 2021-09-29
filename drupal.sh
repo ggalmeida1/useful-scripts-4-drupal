6d41dd8ee955c31fa7a00a5e0d29e201b551b362
Gabriel Almeida <48727389+ggalmeida1@users.noreply.github.com>
2021-09-29 20:20:08 -0300

    com formatação

--- drupal.sh
+++ drupal.sh
@@ -17,7 +17,7 @@
 
 #Mudando para diretorio HOME
 
- echo "Instalando dependencias necessárias para o script. Por gentileza, digitar sua senha sempre que solicitado."
+ echo "^[[31m^[[1mInstalando dependencias necessárias para o script. Por gentileza, digitar sua senha sempre que solicitado.^[[0m"
 
  cd ~/Downloads
 
@@ -30,35 +30,35 @@
 ########------------- Instalação do Docker ---------------#########
 
 
-echo "Inicializando a instalação do Docker"
+echo "^[[31m^[[1mInicializando a instalação do Docker^[[0m"
                                        
 
-echo "Instalando alguns pacotes pré-requisito que deixam o apt usar pacotes pelo HTTPS:\n"
+echo "^[[31m^[[1mInstalando alguns pacotes pré-requisito que deixam o apt usar pacotes pelo HTTPS:\n^[[0m"
 sudo apt install apt-transport-https ca-certificates software-properties-common
 
-echo "Adicionando a chave GPG para o repositório oficial do Docker no seu sistema:\n"
+echo "^[[31m^[[1mAdicionando a chave GPG para o repositório oficial do Docker no seu sistema:\n^[[0m"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 
-echo "Adicionando o repositório do Docker às fontes do APT:\n"
+echo "^[[31m^[[1mAdicionando o repositório do Docker às fontes do APT:\n^[[0m"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 
-echo "Atualizando o banco de dados do pacote com os pacotes do Docker do recém adicionado repositório:\n"
+echo "^[[31m^[[1mAtualizando o banco de dados do pacote com os pacotes do Docker do recém adicionado repositório:\n^[[0m"
 sudo apt update
 
-echo "Checando que você está prestes a instalar do repositório do Docker ao invés do repositório padrão do Ubuntu:\n"
+echo "^[[31m^[[1mChecando que você está prestes a instalar do repositório do Docker ao invés do repositório padrão do Ubuntu:\n^[[0m"
 sudo apt-cache policy docker-ce  | head -n 3 
 
 
-echo "Iniciando a Instalação do Docker: \n"
+echo "^[[31m^[[1mIniciando a Instalação do Docker: \n^[[0m"
 sudo apt install docker-ce
 
-echo "O Docker agora será instalado, o daemon iniciado e o processo habilitado a iniciar no boot. Verificando se ele está funcionando:\n"
+echo "^[[31m^[[1mO Docker agora será instalado, o daemon iniciado e o processo habilitado a iniciar no boot. Verificando se ele está funcionando:\n^[[0m"
 service docker status | head -n5
 
 
-echo "Iniciando a configuração para executar o Comando Docker Sem Sudo\n"
+echo "^[[31m^[[1mIniciando a configuração para executar o Comando Docker Sem Sudo\n^[[0m"
 #evitar digitar  sempre que você executar o comando docker, adicione seu nome de usuário no grupo docker:
 
 sudo usermod -aG docker ${USER}
@@ -68,13 +68,13 @@ sudo usermod -aG docker ${USER}
 
 id -nG
 
-echo "Verificando a instalação:\n"
+echo "^[[31m^[[1mVerificando a instalação:\n^[[0m"
 docker --version
 
-echo "Docker instalado!! \n\n"
+echo "^[[31m^[[1mDocker instalado!! \n\n^[[0m"
 
-echo "####################################################################################################"
-echo "Iniciando a instalação do Lando\n\n"
+echo "^[[31m^[[1m####################################################################################################^[[0m"
+echo "^[[31m^[[1mIniciando a instalação do Lando\n\n^[[0m"
 
 cd /tmp
 curl -s https://api.github.com/repos/lando/lando/releases/latest | grep browser_download_url | grep x64 | grep .deb | cut -d '"' -f 4 | wget -qi -
@@ -82,20 +82,20 @@ sudo dpkg -i lando*.deb
 rm lando*
 cd ~
 
-echo "Foi instalada a ultima versão do Lando!"
+echo "^[[31m^[[1mFoi instalada a ultima versão do Lando!^[[0m"
 lando version
 
-echo "####################################################################################################"
+echo "^[[31m^[[1m####################################################################################################^[[0m"
 
 
-echo "Iniciando a instalação do VSCode\n\n"
+echo "^[[31m^[[1mIniciando a instalação do VSCode\n\n^[[0m"
 cd /tmp
 curl -o code.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
 sudo dpkg -i code.deb
 rm code.deb
 cd ~
 
-echo "Instalando as extensões necessárias: \n\n"
+echo "^[[31m^[[1mInstalando as extensões necessárias: \n\n^[[0m"
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ikappas.composer
 code --install-extension eiminsasete.apacheconf-snippets
@@ -110,15 +110,15 @@ code --install-extension neilbrayfield.php-docblocker
 
 
 ##ATENCAO: Não é possivel instalar as extensões do chrome via terminal: https://stackoverflow.com/questions/16800696/how-install-crx-chrome-extension-via-command-line
-echo "#################################################################################"
-echo "Iniciando a instalação do PHP\n\n"
+echo "^[[31m^[[1m#################################################################################^[[0m"
+echo "^[[31m^[[1mIniciando a instalação do PHP\n\n^[[0m"
 
 sudo apt-get update
 sudo apt-get install php7.4 php7.4-cli php7.4-mbstring php-xml
 
 
-echo "#################################################################################"
-echo "Iniciando a instalação do Composer\n\n"
+echo "^[[31m^[[1m#################################################################################^[[0m"
+echo "^[[31m^[[1mIniciando a instalação do Composer\n\n^[[0m"
 
 #https://getcomposer.org/download/
 
@@ -130,31 +130,29 @@ php -r "unlink('composer-setup.php');"
 
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "Composer foi instalado com sucesso! \n"
+echo "^[[31m^[[1mComposer foi instalado com sucesso! \n^[[0m"
 
 composer -V
 
 
-echo "#################################################################################"
-echo "Iniciando a instalação do PHPCS\n\n"
+echo "^[[31m^[[1m#################################################################################^[[0m"
+echo "^[[31m^[[1mIniciando a instalação do PHPCS\n\n^[[0m"
 
 
 composer global require drupal/coder dealerdirect/phpcodesniffer-composer-installer
 
-
-phpcs -i | grep Drupal
 
 cd /usr/local/bin
 
 sudo ln -s $HOME/.config/composer/vendor/squizlabs/php_codesniffer/bin/phpcs
 sudo ln -s $HOME/.config/composer/vendor/squizlabs/php_codesniffer/bin/phpcbf
 
-echo "Verificando a instalação: \n"
+echo "^[[31m^[[1mVerificando a instalação: \n^[[0m"
 
 phpcs --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer
 
-echo "#################################################################################"
-echo "Concluído!"
+echo "^[[31m^[[1m#################################################################################^[[0m"
+echo "^[[31m^[[1mConcluído!^[[0m"
 exit
 
 
