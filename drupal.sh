15c2c7d9ab18ec08404692709cf0e6c41ea83fdb
Gabriel Almeida <48727389+ggalmeida1@users.noreply.github.com>
2021-09-30 10:12:14 -0300

    English version with improvements

--- drupal.sh
+++ drupal.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-#####	NOME:			drupal.sh
-#####	VERSAO:			1.0
-#####	DESCRICAO:		Script destinado a preparar seu ambiente Linux para Desenvolver em projetos Drupal. Este script fará a instalação 
-#####				de seu ambiente seguindo as melhores práticas de acordo com os padrões estabelecidos pelo Drupal Competence Office (DCO). 			
-#####	DATA DA CRIACAO:	29/09/2021
-#####	ESCRITO POR:		Gabriel Almeida
-#####	E-MAIL:			gabrielda@ciandt.com 			
-#####	DISTRO:			Ubuntu 20.04	
-#####	LICENCA:		GPLv3 			
-#####	PROJETO:                Drupal Setup Environment
-#####   DOCUMENTACAO ORIGINAL:  https://docs.google.com/document/d/1hKdd0Tq05STi14jr4ARL2DvoK4VnMSOLXOrSjBQtePQ/edit 
+#####	NAME:			             drupal.sh
+#####	VERSION:			         1.0.1
+#####	DESCRIPTION:	           	 This script will prepare your environment (Linux Ubuntu) getting you ready to develop in Drupal Projects. It will install  
+#####				    your environment following best practices accordingly to the previously standards established by Drupal Competence Office (DCO). 			
+#####	CREATION DATE:	             29/09/2021
+#####   LAST UPDATE:                 30/09/2021
+#####	AUTHOR:     		         Gabriel Almeida
+#####	E-MAIL:		    	         gabrielda@ciandt.com 			
+#####	LINUX DISTRO:	             Ubuntu 20.04	
+#####	LICENCE:		             GPLv3 			
+#####	PROJECT:                     Drupal Setup Environment
+#####   ORIGINAL DOCUMENTATION:      https://docs.google.com/document/d/1hKdd0Tq05STi14jr4ARL2DvoK4VnMSOLXOrSjBQtePQ/edit 
 
 
 
+#####   NEXT IMPROVEMENT: ALLOW USER TO DECIDE WICH VERSION OF PHP WILL BE INSTALLED.
+#####   STATUS: APPROVED. WORK IN PROGRESS.
+#####   ETA FOR RELEASE: 10/10/21
 
-#Mudando para diretorio HOME
 
- echo "\033[1mInstalando dependencias necessárias para o script. Por gentileza, digitar sua senha sempre que solicitado."
+#Changing to HOME directory
+
+ echo "\033[1mInstalling necessary dependencies... Please, enter your password when asked."
 
  cd ~/Downloads
 
@@ -27,54 +32,54 @@
  sudo apt install curl
  
 
-########------------- Instalação do Docker ---------------#########
+########------------- Docker Installation ---------------#########
 
 
-echo "\033[1mInicializando a instalação do Docker"
+echo "\033[1mStarting to install Docker"
                                        
 
-echo "\033[1mInstalando alguns pacotes pré-requisito que deixam o apt usar pacotes pelo HTTPS:\n"
+echo "\033[1mInstalling a few packeges to allow apt install via HTTPS:\n"
 sudo apt install apt-transport-https ca-certificates software-properties-common
 
-echo "\033[1mAdicionando a chave GPG para o repositório oficial do Docker no seu sistema:\n"
+echo "\033[1mAdding GPG key to Docker's official repository:\n"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 
-echo "\033[1mAdicionando o repositório do Docker às fontes do APT:\n"
+echo "\033[1mAdding Docker's repository to apt sources:\n"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 
-echo "\033[1mAtualizando o banco de dados do pacote com os pacotes do Docker do recém adicionado repositório:\n"
+echo "\033[1mUpdating apt database with Docker's packages from the recently added repo:\n"
 sudo apt update
 
-echo "\033[1mChecando que você está prestes a instalar do repositório do Docker ao invés do repositório padrão do Ubuntu:\n"
+echo "\033[1mChecking that you are ready to install from Docker's repo instead of Ubuntu's standard repo:\n"
 sudo apt-cache policy docker-ce  | head -n 3 
 
 
-echo "\033[1mIniciando a Instalação do Docker: \n"
+echo "\033[1mStarting to install Docker: \n"
 sudo apt install docker-ce
 
-echo "\033[1mO Docker agora será instalado, o daemon iniciado e o processo habilitado a iniciar no boot. Verificando se ele está funcionando:\n"
+echo "\033[1mDocker will now be installed, its deamon started and the process will be iniciated on boot. Verifying if it is working:\n"
 service docker status | head -n5
 
 
-echo "\033[1mIniciando a configuração para executar o Comando Docker Sem Sudo\n"
-#evitar digitar  sempre que você executar o comando docker, adicione seu nome de usuário no grupo docker:
+echo "\033[1mConfiguring Docker to run without sudo\n"
+#It allows you to type whenever you execute docker command, adding your username in docker group:
 
 sudo usermod -aG docker ${USER}
 
 
-#Confirme que seu usuário agora está adicionado ao grupo docker digitando:
+#Verify that your username is now added to docker group:
 
 id -nG
 
-echo "\033[1mVerificando a instalação:\n"
+echo "\033[1mVerifying the install:\n"
 docker --version
 
-echo "\033[1mDocker instalado!! \n\n"
+echo "\033[1mDocker installed successfully!!\n\n"
 
 echo "\033[1m####################################################################################################"
-echo "\033[1mIniciando a instalação do Lando\n\n"
+echo "\033[1mStarting Lando install:\n\n"
 
 cd /tmp
 curl -s https://api.github.com/repos/lando/lando/releases/latest | grep browser_download_url | grep x64 | grep .deb | cut -d '"' -f 4 | wget -qi -
@@ -82,20 +87,20 @@ sudo dpkg -i lando*.deb
 rm lando*
 cd ~
 
-echo "\033[1mFoi instalada a ultima versão do Lando!"
+echo "\033[1mLando's latest version is now installed!"
 lando version
 
 echo "\033[1m####################################################################################################"
 
 
-echo "\033[1mIniciando a instalação do VSCode\n\n"
+echo "\033[1mStarting VSCode installation:\n\n"
 cd /tmp
 curl -o code.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
 sudo dpkg -i code.deb
 rm code.deb
 cd ~
 
-echo "\033[1mInstalando as extensões necessárias: \n\n"
+echo "\033[1mInstalling VSCode extensions: \n\n"
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ikappas.composer
 code --install-extension eiminsasete.apacheconf-snippets
@@ -109,16 +114,16 @@ code --install-extension ikappas.phpcs
 code --install-extension neilbrayfield.php-docblocker
 
 
-##ATENCAO: Não é possivel instalar as extensões do chrome via terminal: https://stackoverflow.com/questions/16800696/how-install-crx-chrome-extension-via-command-line
+##ATTENTION: It's not possible to install Chrome extension via shell according to this thread: https://stackoverflow.com/questions/16800696/how-install-crx-chrome-extension-via-command-line
 echo "\033[1m#################################################################################"
-echo "\033[1mIniciando a instalação do PHP\n\n"
+echo "\033[1mWe are now going to install PHP: \n\n"
 
 sudo apt-get update
 sudo apt-get install php7.4 php7.4-cli php7.4-mbstring php-xml
 
 
 echo "\033[1m#################################################################################"
-echo "\033[1mIniciando a instalação do Composer\n\n"
+echo "\033[1mStarting Composer installation: \n\n"
 
 #https://getcomposer.org/download/
 
@@ -130,13 +135,13 @@ php -r "unlink('composer-setup.php');"
 
 sudo mv composer.phar /usr/local/bin/composer
 
-echo "\033[1mComposer foi instalado com sucesso! \n"
+echo "\033[1mComposer was installed successfully! \n"
 
 composer -V
 
 
 echo "\033[1m#################################################################################"
-echo "\033[1mIniciando a instalação do PHPCS\n\n"
+echo "\033[1mStarting to install PHPCS: \n\n"
 
 
 composer global require drupal/coder dealerdirect/phpcodesniffer-composer-installer
@@ -147,12 +152,12 @@ cd /usr/local/bin
 sudo ln -s $HOME/.config/composer/vendor/squizlabs/php_codesniffer/bin/phpcs
 sudo ln -s $HOME/.config/composer/vendor/squizlabs/php_codesniffer/bin/phpcbf
 
-echo "\033[1mVerificando a instalação: \n"
+echo "\033[1mChecking everything is OK: \n"
 
 phpcs --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer
 
 echo "\033[1m#################################################################################"
-echo "\033[1mConcluído!"
+echo "\033[1mIt's all set! You can now develop with Drupal :)"
 exit
 
 
